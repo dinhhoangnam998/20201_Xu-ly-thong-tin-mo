8bd25ad237e14f8bec4db06394e2e3438e3df256
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2021-01-01 11:22:54 +0700

    prepend colname when read excel rule file

---
+++ src/fuzzy_processing/rule-readers.js
@@ -0,0 +1,13 @@
+const XLSX = require("xlsx");
+const workbook = XLSX.readFile("speed_rules.xlsx");
+const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+const rules = XLSX.utils.sheet_to_json(worksheet);
+const newRules = rules.map((rule) => {
+  const keys = Object.keys(rule);
+  const newRule = {};
+  keys.forEach((key) => {
+    newRule[key] = key + "_" + rule[key];
+  });
+  return newRule;
+});
+console.log(newRules);
