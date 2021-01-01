6fff71f94f3648a7eee55b8d107edc1093aa32dd
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2021-01-01 21:11:29 +0700

    refactor code,

--- src/fuzzy_processing/rule-readers.js
+++ src/fuzzy_processing/rule-readers.js
@@ -1,13 +1,18 @@
 const XLSX = require("xlsx");
-const workbook = XLSX.readFile("speed_rules.xlsx");
-const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-const rules = XLSX.utils.sheet_to_json(worksheet);
-const newRules = rules.map((rule) => {
-  const keys = Object.keys(rule);
-  const newRule = {};
-  keys.forEach((key) => {
-    newRule[key] = key + "_" + rule[key];
+
+function readSpeedRules() {
+  const workbook = XLSX.readFile("speed_rules.xlsx");
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const rules = XLSX.utils.sheet_to_json(worksheet);
+  const newRules = rules.map((rule) => {
+    const keys = Object.keys(rule);
+    const newRule = {};
+    keys.forEach((key) => {
+      newRule[key] = key + "_" + rule[key];
+    });
+    return newRule;
   });
-  return newRule;
-});
-console.log(newRules);
+  // console.log(newRules);
+  return newRules;
+}
+module.exports = { readSpeedRules };
