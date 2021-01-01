d560c2a83cfa8ee58ba16effb344da18913488da
Dinh-Hoang Nam <dinhhoangnam162793@gmail.com>
2021-01-01 11:22:28 +0700

    rename variable

--- src/fuzzy_processing/fuzzy-sets.js
+++ src/fuzzy_processing/fuzzy-sets.js
@@ -26,7 +26,7 @@ const {
 } = require("./constance");
 const { makeLeftTrapezoid, makeTriangle, makeRightTrapezoid, makeTrapezoid } = require("./make-fuzzy-set-utils");
 
-const deviation = [
+const deviationFuzzySets = [
   { name: DEVIATION_FAR_LEFT, muy: makeLeftTrapezoid(0.25, 0.4) },
   { name: DEVIATION_LEFT, muy: makeTriangle(0.25, 0.4, 0.5) },
   { name: DEVIATION_MIDDLE, muy: makeTriangle(0.4, 0.5, 0.6) },
@@ -34,7 +34,7 @@ const deviation = [
   { name: DEVIATION_FAR_RIGHT, muy: makeRightTrapezoid(0.6, 0.75) },
 ];
 
-const steering = [
+const steeringFuzzySets = [
   { name: STEERING_HARD_LEFT, muy: makeLeftTrapezoid(0.25, 0.4) },
   { name: STEERING_LEFT, muy: makeTriangle(0.25, 0.4, 0.5) },
   { name: STEERING_STRAIGHT, muy: makeTriangle(0.4, 0.5, 0.6) },
@@ -42,7 +42,7 @@ const steering = [
   { name: STEERING_HARD_RIGHT, muy: makeRightTrapezoid(0.6, 0.75) },
 ];
 
-const lightStatus = [
+const lightStatusFuzzySets = [
   { name: LIGHT_STATUS_GREEN, muy: makeLeftTrapezoid(0.25, 0.4) },
   { name: LIGHT_STATUS_GREEN, muy: makeTriangle(0.32, 0.42, 0.54) },
   { name: LIGHT_STATUS_YELLOW, muy: makeTrapezoid(0.44, 0.54, 0.72) },
@@ -50,23 +50,23 @@ const lightStatus = [
   { name: LIGHT_STATUS_LESS_RED, muy: makeRightTrapezoid(0.83, 0.92) },
 ];
 
-const distanceLight = [
+const distanceLightFuzzySets = [
   { name: DISTANCE_LIGHT_NEAR, muy: makeLeftTrapezoid(0.05, 0.2) },
   { name: DISTANCE_LIGHT_MEDIUM, muy: makeTrapezoid(0.05, 0.1, 0.25, 0.35) },
   { name: DISTANCE_LIGHT_FAR, muy: makeRightTrapezoid(0.2, 0.35) },
 ];
 
-const distanceObstacle = [
-  { name: DISTANCE_OBSTACLE_NEAR, muy: makeTrapezoid(0.05, 0.2) },
+const distanceObstacleFuzzySets = [
+  { name: DISTANCE_OBSTACLE_NEAR, muy: makeLeftTrapezoid(0.05, 0.2) },
   { name: DISTANCE_OBSTACLE_MEDIUM, muy: makeTrapezoid(0.05, 0.1, 0.25, 0.35) },
   { name: DISTANCE_OBSTACLE_FAR, muy: makeRightTrapezoid(0.2, 0.35) },
 ];
 
-const speed = [
+const speedFuzzySets = [
   { name: SPEED_STOP, muy: makeLeftTrapezoid(0, 0.05) },
   { name: SPEED_SLOWER, muy: makeTriangle(0.025, 0.25, 0.525) },
   { name: SPEED_SLOW, muy: makeTriangle(0.3, 0.6, 0.8) },
   { name: SPEED_MEDIUM, muy: makeRightTrapezoid(0.7, 0.9) },
 ];
 
-module.exports = { deviation, steering, lightStatus, distanceLight, distanceObstacle, speed };
+module.exports = { deviationFuzzySets, steeringFuzzySets, lightStatusFuzzySets, distanceLightFuzzySets, distanceObstacleFuzzySets, speedFuzzySets };
